5451a1f4ae21c282697c3c39b6b4a580fcade7e3
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-09-13 12:10:59 -0300

    chore: inserção de pacotes

---
+++ produtos.js
@@ -0,0 +1,87 @@
+products = [
+    {
+        productName:"x",
+        price:3200,
+        descryption: "bolinha,bolinha,boinha",
+        image: 'https://imgur.com/a/WAzvvJT',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/OGk7F4r',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/2zO6g9N',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/eOGlwfu',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/xV3AQU7',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/10kW52u',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/eOGlwfu',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/WAzvvJT',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/xV3AQU7',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/xV3AQU7',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/WAzvvJT',
+
+    },
+    {
+        productName:"x",
+        price:200,
+        descryption: "bolinha,bolinha",
+        image: 'https://imgur.com/a/eOGlwfu',
+
+    }
+
+]
